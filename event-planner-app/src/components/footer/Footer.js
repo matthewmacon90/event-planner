717041136f9c4ad86662f699743f60aa6ef8ae7c
Matthew Macon <matthewmacon90@gmail.com>
2024-08-08 19:42:24 -0400

    changed color scheme, updated footer

--- event-planner-app/src/components/footer/Footer.js
+++ event-planner-app/src/components/footer/Footer.js
@@ -3,7 +3,17 @@ import './FooterStyles.css';
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <p>© 2021 - All rights reserved</p>
+            <div className='footer-leftside'>Left Side</div>
+            <div className='footer-middle'>
+                <p>© 2021 - All rights reserved</p>
+            </div>
+            <div className='footer-rightside'>
+                <div className='footer-social'>
+                    <a>FACEBOOK</a>
+                    <a>INSTAGRAM</a>
+                    <a>TWITTER</a>
+                </div>
+            </div>
         </footer>
     )
 };
--- event-planner-app/src/pages/home/home-components/Advertisement.js
+++ event-planner-app/src/pages/home/home-components/Advertisement.js
@@ -2,9 +2,9 @@ const Advertisement = () => {
     return (
         <div className="advertisement-container">
             <div className="advertisement-one">
-                <h2>Advertisement</h2>
-                <p>Advertise with us today!</p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h2>About Me</h2>
+                <p>Welcome! My name is Natalie, and I’m delighted you’ve discovered my page. As a seasoned event coordinator based in the vibrant town of Madison, Indiana, I have a passion for orchestrating unforgettable music events and fostering a thriving arts scene. My journey has been one of collaboration, working hand-in-hand with local businesses to curate experiences that resonate with our community’s spirit.</p>
+                <button className="about-me-home-btn">Click to learn more!</button>
             </div>
             <div className="advertisement-two">
                 <h2>Advertisement</h2>

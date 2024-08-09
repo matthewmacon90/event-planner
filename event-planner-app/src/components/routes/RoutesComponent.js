9ac4877ddbe9885ded7bb96e8256d30577e3f433
Matthew Macon <matthewmacon90@gmail.com>
2024-08-09 13:58:01 -0400

    created about page

--- event-planner-app/src/components/routes/RoutesComponent.js
+++ event-planner-app/src/components/routes/RoutesComponent.js
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 import HomePage from '../../pages/home/HomePage';
+import AboutPage from '../../pages/about/AboutPage';
 import ContactPage from '../../pages/contact/ContactPage';
 
 const RoutesComponent = () => {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
             <Route path='/contact' element={<ContactPage />} />
         </Routes>
     );
